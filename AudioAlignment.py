61f8cb516d36ad310050c50fa4fc45a27814270c
Sfencer09 <treystevens00@gmail.com>
2024-03-14 20:01:26 -0400

    Split finding all offsets into own function for reuse

--- AudioAlignment.py
+++ AudioAlignment.py
@@ -1,4 +1,5 @@
 from math import ceil
+import time
 from typing import Dict, List, Tuple
 import librosa
 import numpy as np
@@ -121,8 +122,7 @@ def findAudioOffset(
 def histogramByBucket(arr, bucketSize = 10):
     ...
 
-def findAverageAudioOffset(
-    within_file: str,
+def findAudioOffsets(within_file: str,
     find_file: str,
     initialOffset: float = 0,
     start: float = 0,
@@ -130,17 +130,23 @@ def findAverageAudioOffset(
     macroWindowSize: int = 3600,
     macroStride: int | None = None,
     microWindowSize: int = 60,
-    microStride: float | None = 30,
-):
+    microStride: float | int | None = 30,
+    bucketSize: float | int = 1,
+    bucketSpillover: int = 0,
+    ):
+    startTime = time.time()
     if macroWindowSize < 5 * microWindowSize:
         raise ValueError("macroWindowSize should be at least five times microWindowSize for good results")
+    if macroWindowSize < 60 * 10:
+        raise ValueError("macroWindowSize must be at least 10 minutes")
     if macroStride is None:
-        macroStride = macroWindowSize
+        macroStride = macroWindowSize // 2
     if microStride is None:
-        microStride = microWindowSize
+        microStride = microWindowSize // 2
     withinAudioFile = extractAudio(within_file)
     findAudioFile = extractAudio(find_file)
     print("Audio extracted, memory tuple:", psutil.virtual_memory())
+    print("Initial offset =", initialOffset)
     y_within, sr_within = librosa.load(
         withinAudioFile,
         sr=None,
@@ -159,10 +165,8 @@ def findAverageAudioOffset(
     withinLength = y_within.shape[0] / sr_within
     findLength = y_find.shape[0] / sr_within
     overlapLength = min(withinLength, findLength)
-    offsetsFound: Dict[str, List[float]] = dict()
-    threshold = 500
-    bucketSize: int = 1
-    bucketSpillover: int = 1
+    offsetsFound: Dict[str, List[Tuple[float, float]]] = dict()
+    threshold = max(100, 5 * microWindowSize) #500
     for macroWindowNum in range(int(ceil((overlapLength - macroWindowSize) / macroStride))):
         macroWindowStart = macroWindowNum * macroStride * sr_within
         macroWindowEnd = ((macroWindowNum * macroStride) + macroWindowSize) * sr_within
@@ -173,31 +177,82 @@ def findAverageAudioOffset(
             microWindowEnd = macroWindowStart + (((microWindowNum * microStride) + microWindowSize) * sr_within)
             print("Micro start:", microWindowStart/sr_within, "end:", microWindowEnd/sr_within)
             findSnippet = y_find[microWindowStart:microWindowEnd]
-            c = signal.correlate(y_within, findSnippet, mode='same', method='fft')
+            c = signal.correlate(withinSnippet, findSnippet, mode='same', method='fft')
             print("Signal correlated, memory tuple:", psutil.virtual_memory())
             peak = np.argmax(c)
+            print("within shape =", withinSnippet.shape, ", find shape =", findSnippet.shape, ", c shape =", c.shape)
+            foundOffset = round(((peak + macroWindowStart - microWindowStart) / sr_within) - (microWindowSize / 2), 2)
             if c[peak] >= threshold:
-                offsetFound = round((peak - macroWindowStart) / sr_within, 2)
-                offsetStr = str(int(offsetFound // bucketSize) * bucketSize)
-                print(f"Found offset {offsetFound}, putting in bucket {offsetStr}")
+                offsetStr = str(round(foundOffset / bucketSize) * bucketSize)
+                print(f"Found offset {foundOffset}, putting in bucket {offsetStr} (peakHeight={c[peak]}, threshold={threshold}, peakTimeInternal={peak/sr_within})")
                 if offsetStr in offsetsFound.keys():
-                    offsetsFound[offsetStr].append(offsetFound)
+                    offsetsFound[offsetStr].append((foundOffset, microWindowStart))
                 else:
-                    offsetsFound[offsetStr] = [offsetFound]
-
+                    offsetsFound[offsetStr] = [(foundOffset, microWindowStart)]
+            else:
+                print(f"Found offset {foundOffset} with insufficient peak match (peakHeight={c[peak]}, threshold={threshold}, peakTimeInternal={peak/sr_within})")
+    print("Time =", time.time() - startTime, "seconds")
+    print(offsetsFound)
+    print("\n\n\n")
     if len(offsetsFound) == 0:
         return None
-    offsetsByFrequency = sorted(offsetFound.keys(), key=lambda x: len(offsetFound[x]))
+    spilledOffsets: Dict[str, List[float]] = dict()
+    for key in offsetsFound.keys():
+        spilledOffsets[key] = list(offsetsFound[key])
+        keyInt = float(key)
+        for offset in range(0, bucketSpillover):
+            upOneKey = str(keyInt+(bucketSize*offset))
+            downOneKey = str(keyInt-(bucketSize*offset))
+            print(key, upOneKey, downOneKey)
+            if upOneKey in offsetsFound.keys():
+                spilledOffsets[key].extend(offsetsFound[upOneKey])
+            if downOneKey in offsetsFound.keys():
+                spilledOffsets[key].extend(offsetsFound[downOneKey])
+    print(spilledOffsets)
+    return spilledOffsets
+
+
+def findAverageAudioOffset(
+    within_file: str,
+    find_file: str,
+    initialOffset: float = 0,
+    start: float = 0,
+    duration: float | None = None,
+    macroWindowSize: int = 3600,
+    macroStride: int | None = None,
+    microWindowSize: int = 60,
+    microStride: float | int | None = 30,
+    bucketSize: float | int = 1,
+    bucketSpillover: int = 0,
+):
+    allOffsets = findAudioOffsets(within_file=within_file,
+                                      find_file=find_file,
+                                      initialOffset=initialOffset,
+                                      start=start,
+                                      duration=duration,
+                                      macroWindowSize=macroWindowSize,
+                                      macroStride=macroStride,
+                                      microWindowSize=microWindowSize,
+                                      microStride=microStride,
+                                      bucketSize=bucketSize,
+                                      bucketSpillover=bucketSpillover,
+                                      )
+    offsetsByFrequency = sorted(allOffsets.keys(), key=lambda x: -len(allOffsets[x]))
     print(offsetsByFrequency)
-    return np.average(offsetsByFrequency[0])
+    if len(offsetsByFrequency) == 0:
+        return None
+    popularOffsets = [offset for offset in offsetsByFrequency if len(allOffsets[offset]) > 1]
+    print(popularOffsets)
+    mostPopularOffset = offsetsByFrequency[0]
+    print(mostPopularOffset)
+    return sum((offset for offset, _ in allOffsets[mostPopularOffset])) / len(allOffsets[mostPopularOffset])
     
 
 # First element of return value will be initial offset, all other elements will be a tuple of the start time and time difference for a later offset
 def findFileOffset(
     file1: SourceFile,
     file2: SourceFile,
-    duration: float | None = __DEFAULT_DURATION,
-    window: float | None = __DEFAULT_WINDOW,
+    **kwargs
 ) -> List[float | Tuple[float, float]]:
     file1Start = file1.infoJson["timestamp"]
     file2Start = file2.infoJson["timestamp"]
@@ -208,4 +263,4 @@ def findFileOffset(
     file2Path = (
         file2.localVideoFile if file2.localVideoFile is not None else file2.videoFile
     )
-    return findAudioOffset(file1Path, file2Path, offset, duration, window)
+    return findAverageAudioOffset(file1Path, file2Path, offset, **kwargs)
