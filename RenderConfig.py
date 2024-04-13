435bebe97a39a9fbc4b4ffbe55177a9b28481333
Sfencer09 <treystevens00@gmail.com>
2024-04-12 23:48:13 -0400

    Finish updating RenderConfig.py to use new config module

--- RenderConfig.py
+++ RenderConfig.py
@@ -8,60 +8,63 @@ from schema import Schema, Or, And, Optional, Use
 from MTRLogging import getLogger
 logger = getLogger('RenderConfig')
 
-if __debug__:
-    from config import *
-exec(open("config.py").read(), globals())
+#if __debug__:
+#    from config import *
+#exec(open("config.py").read(), globals())
 
+from MTRConfig import trueStrings, getConfig, isAcceptedOutputCodec, isHardwareOutputCodec, validateHwaccelFunctions, HW_DECODE, HW_ENCODE, HW_INPUT_SCALE, HW_OUTPUT_SCALE
+
+defaultRenderConfig = getConfig('main.defaultRenderConfig')
 
 renderConfigSchema = Schema({
     Optional('drawLabels', default=defaultRenderConfig['drawLabels']):
-    Or(bool, Use(lambda x: x.lower() in trueStrings)),
+        Or(bool, Use(lambda x: x.lower() in trueStrings)),
     Optional('startTimeMode', default=defaultRenderConfig['startTimeMode']):
-    lambda x: x in ('mainSessionStart', 'allOverlapStart'),
+        lambda x: x in ('mainSessionStart', 'allOverlapStart'),
     Optional('endTimeMode', default=defaultRenderConfig['endTimeMode']):
-    lambda x: x in ('mainSessionEnd', 'allOverlapEnd'),
+        lambda x: x in ('mainSessionEnd', 'allOverlapEnd'),
     Optional('logLevel', default=defaultRenderConfig['logLevel']):
-    And(Use(int), lambda x: 0 <= x <= 4),  # max logLevel = 4
+        And(Use(int), lambda x: 0 <= x <= 4),  # max logLevel = 4
     Optional('sessionTrimLookback', default=defaultRenderConfig['sessionTrimLookback']):
-    # TODO: convert from number of segments to number of seconds. Same for lookahead
-    Use(int),
+        # TODO: convert from number of segments to number of seconds. Same for lookahead
+        Use(int),
     Optional('sessionTrimLookahead', default=defaultRenderConfig['sessionTrimLookahead']):
-    And(Use(int), lambda x: x >= 0),
+        And(Use(int), lambda x: x >= 0),
     Optional('sessionTrimLookbackSeconds', default=defaultRenderConfig['sessionTrimLookbackSeconds']):
-    And(Use(int), lambda x: x >= 0),  # Not implemented yet
+        And(Use(int), lambda x: x >= 0),  # Not implemented yet
     Optional('sessionTrimLookaheadSeconds', default=defaultRenderConfig['sessionTrimLookaheadSeconds']):
-    And(Use(int), lambda x: x >= 0),
+        And(Use(int), lambda x: x >= 0),
     # Optional(Or(Optional('sessionTrimLookback', default=0),
     # Optional('sessionTrimLookbackSeconds', default=0), only_one=True), ''): And(int, lambda x: x>=-1),
     # Optional(Or(Optional('sessionTrimLookahead', default=0),
     # Optional('sessionTrimLookaheadSeconds', default=600), only_one=True): And(int, lambda x: x>=0),
     Optional('minGapSize', default=defaultRenderConfig['minGapSize']):
-    And(Use(int), lambda x: x >= 0),
+        And(Use(int), lambda x: x >= 0),
     Optional('outputCodec', default=defaultRenderConfig['outputCodec']):
-    lambda x: x in acceptedOutputCodecs,
+        isAcceptedOutputCodec,
     Optional('encodingSpeedPreset', default=defaultRenderConfig['encodingSpeedPreset']):
-    lambda x: x in ('ultrafast', 'superfast', 'veryfast', 'faster', 'fast', 'medium',
+        lambda x: x in ('ultrafast', 'superfast', 'veryfast', 'faster', 'fast', 'medium',
                     'slow', 'slower', 'veryslow') or x in [f'p{i}' for i in range(1, 8)],
     Optional('useHardwareAcceleration', default=defaultRenderConfig['useHardwareAcceleration']):
-    And(Use(int), lambda x: x & HWACCEL_FUNCTIONS == x),
-    # And(Use(int), lambda x: 0 <= x < 16), #bitmask; 0=None, bit 1(1)=decode, bit 2(2)=scale input, bit 3(4)=scale output, bit 4(8)=encode
+        And(Use(int), validateHwaccelFunctions),
+        # And(Use(int), lambda x: 0 <= x < 16), #bitmask; 0=None, bit 1(1)=decode, bit 2(2)=scale input, bit 3(4)=scale output, bit 4(8)=encode
     Optional('maxHwaccelFiles', default=defaultRenderConfig['maxHwaccelFiles']):
-    And(Use(int), lambda x: x >= 0),
+        And(Use(int), lambda x: x >= 0),
     Optional('minimumTimeInVideo', default=defaultRenderConfig['minimumTimeInVideo']):
-    And(Use(int), lambda x: x >= 0),
+        And(Use(int), lambda x: x >= 0),
     Optional('cutMode', default=defaultRenderConfig['cutMode']):
-    lambda x: x in ('chunked', ),  # 'trim', 'segment'),
+        lambda x: x in ('chunked', ),  # 'trim', 'segment'),
     Optional('useChat', default=defaultRenderConfig['useChat']):
-    Or(bool, Use(lambda x: x.lower() in trueStrings)),
+        Or(bool, Use(lambda x: x.lower() in trueStrings)),
     # overrides chat, but will not prevent game matching
     Optional('includeStreamers', default=None):
-    # Cannot be passed as string
-    Or(lambda x: x is None, [str], {str: Or(lambda x: x is None, [str])}),
+        # Cannot be passed as string
+        Or(lambda x: x is None, [str], {str: Or(lambda x: x is None, [str])}),
     Optional('excludeStreamers', default=None):
-    # Cannot be passed as string
-    Or(lambda x: x is None, [str], {str: Or(lambda x: x is None, [str])}),
+        # Cannot be passed as string
+        Or(lambda x: x is None, [str], {str: Or(lambda x: x is None, [str])}),
     Optional('preciseAlign', default=defaultRenderConfig['preciseAlign']):
-    Or(bool, Use(lambda x: x.lower() in trueStrings)),
+        Or(bool, Use(lambda x: x.lower() in trueStrings)),
 })
 
 
@@ -88,7 +91,7 @@ class RenderConfig:
 
     def __init__(self, **kwargs):
         values:dict = renderConfigSchema.validate(kwargs)
-        if values['outputCodec'] in hardwareOutputCodecs:
+        if isHardwareOutputCodec(values['outputCodec']):
             if values['useHardwareAcceleration'] & HW_ENCODE == 0:
                 raise Exception(
                     f"Must enable hardware encoding bit in useHardwareAcceleration if using hardware-accelerated output codec {values['outputCodec']}")
@@ -101,7 +104,7 @@ class RenderConfig:
                 raise Exception(
                     f"Hardware-accelerated output scaling must currently be used with hardware encoding")
         if values['useHardwareAcceleration'] & HW_ENCODE != 0:
-            if values['outputCodec'] not in hardwareOutputCodecs:
+            if not isHardwareOutputCodec(values['outputCodec']):# not in hardwareOutputCodecs:
                 raise Exception(
                     f"Must specify hardware-accelerated output codec if hardware encoding bit in useHardwareAcceleration is enabled")
         for key, value in values.items():
